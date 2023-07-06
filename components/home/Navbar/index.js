17a5fbe50770277bb71e9be43056a142cba36e6f
rianmz-genz <adrianji1945@gmail.com>
2023-07-06 10:05:22 +0700

    homepage navbar

--- components/home/Navbar/index.js
+++ components/home/Navbar/index.js
@@ -1,9 +1,11 @@
+import Logo from '@/components/Logo'
 import React from 'react'
 
 const NavbarLandingpage = () => {
   return (
-    <header className='w-full h-16 fixed px-4 py-2 bg-white shadow top-0 left-0'>
-        <nav className='w-11/12 mx-auto'>
+    <header className='w-full h-20 fixed px-4 flex items-center justify-center bg-white top-0 left-0'>
+      <nav className='w-11/12 mx-auto'>
+        <Logo className={"w-48"} />
             <ul>
                   
             </ul>
