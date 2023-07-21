872a32554a9a0da2abae499adfe6557b98ababba
rianmz-genz <adrianji1945@gmail.com>
2023-07-21 08:00:14 +0700

    responsif

--- components/home/Feature/index.js
+++ components/home/Feature/index.js
@@ -9,7 +9,7 @@ const FeatureSection = () => {
   return (
     <section id='feature' className='w-full py-12 my-12 min-h-screen bg-gradient-to-t from-yellow-600/80 via-yellow-600/20 to-transparent'>
           <SectionParagraph className={'w-8/12 text-center mx-auto'} title={feature.title} description={feature.description} />
-          <ul className='grid grid-cols-3 container mx-auto gap-4 mt-8'>
+          <ul className='grid grid-cols-1 md:grid-cols-3 w-11/12 px-4 mx-auto gap-4 mt-8'>
               {feature.items.map((item, idx) => (
                 <li key={idx} className='rounded-md p-6 bg-white hover:shadow-2xl transition-all duration-300 shadow-xl space-y-4 flex flex-col justify-center items-start'>
                   <div className='bg-yellow-600/10 text-yellow-600 rounded-full p-3 text-xl'>
