8b50884d6d7453edc5e743384071b93aecd0b0c1
rianmz-genz <adrianji1945@gmail.com>
2023-07-21 08:00:41 +0700

    responsif

--- components/home/Navbar/index.js
+++ components/home/Navbar/index.js
@@ -1,35 +1,49 @@
-import Logo from '@/components/Logo'
-import Button from '@/components/globals/Button'
-import { contentData } from '@/store'
-import { buttonStyle } from '@/utils/enum'
-import Link from 'next/link'
-import React from 'react'
-
+import Logo from "@/components/Logo";
+import Button from "@/components/globals/Button";
+import { contentData } from "@/store";
+import { buttonStyle } from "@/utils/enum";
+import Link from "next/link";
+import React, { useState } from "react";
+import { FiX } from "react-icons/fi";
+import {HiOutlineBars3BottomRight} from 'react-icons/hi2'
 const NavbarLandingpage = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <header className='w-full mt-8  flex items-center justify-center bg-white top-0 left-0'>
-      <nav className='w-full mx-auto flex justify-between items-center'>
-        <Logo className={"w-48"} />
-            <ul className='flex w-full justify-evenly items-center'>
-              {contentData.home.navbar.map((item, idx) => (
-              <li key={idx}>
-              <a href={item.path} className='font-semibold text-black/80'>{ item.value }</a>
-                  </li>
-              ))}
-                    <Link href="/auth/login" className='font-semibold border-l pl-8 text-black/80'>
+    <header className="w-full mt-8  flex items-center justify-center bg-white top-0 left-0">
+      <nav className="w-full mx-auto flex lg:flex-row flex-col justify-between lg:items-center ">
+        <div className="flex justify-between items-center">
+          <Logo className={"w-48"} />
+        <button onClick={() => setIsOpen(!isOpen)} className="text-2xl bg-slate-100 p-2 rounded lg:hidden ">
+          {isOpen ? <FiX /> :<HiOutlineBars3BottomRight />}
+        </button>
+        </div>
+        <ul className={`lg:flex lg:flex-row flex-col w-full lg:justify-evenly justify-center items-center bg-white max-lg:shadow px-8 py-8 lg:px-0 lg:py-0 space-y-3 lg:space-y-0 ${isOpen ? "" : "hidden"}`}>
+          {contentData.home.navbar.map((item, idx) => (
+            <li key={idx}>
+              <a href={item.path} className="font-semibold text-black/80">
+                {item.value}
+              </a>
+            </li>
+          ))}
+          <li>
+             <Link
+            href="/auth/login"
+            className="font-semibold lg:border-l lg:pl-8 text-black/80"
+          >
             Login
           </Link>
-          <Link href={'/auth/register'}>
-          <Button style={buttonStyle.outlineprimarylarge}>
-            Buat Undangan
-          </Button>
+         </li>
+          <li>
+            <Link href={"/auth/register"}>
+            <Button style={buttonStyle.outlineprimarylarge}>
+              Buat Undangan
+            </Button>
           </Link>
+          </li>
         </ul>
-
-      
-        </nav>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default NavbarLandingpage
+export default NavbarLandingpage;
