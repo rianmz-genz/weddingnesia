726ead02e14b0e3c7fca3b86950d52289703fbe3
rianmz-genz <adrianji1945@gmail.com>
2023-07-26 22:08:01 +0700

    button variant

--- components/globals/Button.js
+++ components/globals/Button.js
@@ -2,13 +2,15 @@ import React from "react";
 import { buttonStyle } from "@/utils/enum";
 import { variant } from "@/utils";
 const Button = ({ style, className, ...props }) => {
+  const large = "px-8 py-4 font-bold"
   const button = variant(
     `rounded-xl ${className}`,
     {
       style: {
-        primarylarge: 'px-8 py-4 bg-yellow-700/80 text-white font-bold hover:bg-yellow-700/90',
+        primarylarge: `${large} bg-yellow-700/80 text-white hover:bg-yellow-700/90`,
         outlineprimarylarge: 'px-6 py-3 border-2 font-bold border-yellow-700/90 text-yellow-700/90 hover:text-white hover:bg-yellow-700/90',
-        silverlarge: 'px-8 py-4 bg-slate-50/30 hover:ring-2 font-bold hover:ring-black hover:bg-white text-black'
+        silverlarge: `${large} bg-slate-50/30 hover:ring-2 hover:ring-black hover:bg-white text-black`,
+        blackLarge: `${large} bg-black/95 text-yellow-600 hover:bg-black`
       },
     }
   );
