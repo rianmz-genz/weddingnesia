1a4ba1a79a3d5a5bec7544a8a631d3f6fb28db63
rianmz-genz <adrianji1945@gmail.com>
2023-08-03 06:56:41 +0700

    search input bg

--- components/globals/Input.js
+++ components/globals/Input.js
@@ -3,11 +3,14 @@ import React from "react";
 import { FiSearch } from "react-icons/fi";
 
 const Input = ({ style, className, ...props }) => {
-  const inputVariant = variant(`rounded-md focus:outline-none transition-all duration-300 ${className}`, {
+  const inputVariant = variant(
+    `rounded-md focus:outline-none transition-all duration-300 ${className}`,
+    {
       style: {
-        base: 'w-full px-4 py-2 border border-black/10 ring-0 focus:border focus:border-yellow-700'
-    },
-  });
+        base: "w-full px-4 py-2 border border-black/10 ring-0 focus:border focus:border-yellow-700",
+      },
+    }
+  );
   return <input className={inputVariant({ style })} {...props} />;
 };
 Input.defaultProps = {
@@ -15,9 +18,18 @@ Input.defaultProps = {
 };
 export default Input;
 
-export const InputSearch = ({className, ...props}) => {
-  return <div className={`flex items-center justify-center space-x-2 bg-blue-400/10 px-4 py-2 rounded-full ${className}`}>
-              <FiSearch className="text-blue-400" />
-              <input {...props} placeholder='Cari Undangan' className='text-sm focus:outline-none bg-transparent' type="search" />
-            </div>
-}
+export const InputSearch = ({ className, ...props }) => {
+  return (
+    <div
+      className={`flex items-center justify-center space-x-2 shadow shadow-black/5 bg-white px-4 py-2 rounded-full ${className}`}
+    >
+      <FiSearch className="text-black" />
+      <input
+        {...props}
+        placeholder="Cari Undangan"
+        className="text-sm focus:outline-none bg-transparent"
+        type="search"
+      />
+    </div>
+  );
+};
