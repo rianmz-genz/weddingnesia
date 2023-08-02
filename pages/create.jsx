78e5526913da0c14eede08e519dd5ec4a871a4d1
rianmz-genz <adrianji1945@gmail.com>
2023-08-03 06:57:36 +0700

    jsx

---
+++ pages/create.jsx
@@ -0,0 +1,12 @@
+import ProfileCard from "@/components/dashboard/user/profilecard";
+import React from "react";
+
+export default function Create() {
+  return (
+    <div className="w-full min-h-screen bg-slate-50 py-12">
+      <div className="w-11/12 mx-auto">
+        <ProfileCard />
+      </div>
+    </div>
+  );
+}
