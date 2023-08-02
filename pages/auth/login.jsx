bf55386b3ed7c2ec5c8f2a22541c0e39535a8060
rianmz-genz <adrianji1945@gmail.com>
2023-08-03 06:57:56 +0700

    jsx

---
+++ pages/auth/login.jsx
@@ -0,0 +1,50 @@
+import { AuthPage, Button, InputIcon, Logo, Text } from "@/components";
+import { textStyle } from "@/utils/enum";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { BiUser } from "react-icons/bi";
+import { FiKey } from "react-icons/fi";
+
+const LoginView = () => {
+  const router = useRouter();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    router.push("/dashboard");
+  };
+  return (
+    <AuthPage onSubmit={handleLogin}>
+      <Logo className={"w-8/12"} />
+      <div className="w-full mt-6">
+        <Text className={"mb-1"} style={textStyle.titleQuestion}>
+          Email
+        </Text>
+        <InputIcon
+          autoFocus={true}
+          placeholder="Email"
+          icon={<BiUser className="text-black mr-2" />}
+        />
+        <Text className={"mb-1 mt-3"} style={textStyle.titleQuestion}>
+          Password
+        </Text>
+        <InputIcon
+          type="password"
+          placeholder="Password"
+          icon={<FiKey className="text-black mr-2" />}
+        />
+        <Button className={"mt-6 w-full"}>Masuk</Button>
+        <Text className={"text-center mt-3"}>
+          Belum memiliki akun?{" "}
+          <Link
+            href={"/auth/register"}
+            className="font-semibold underline text-yellow-700/80"
+          >
+            Daftar
+          </Link>
+        </Text>
+      </div>
+    </AuthPage>
+  );
+};
+
+export default LoginView;
