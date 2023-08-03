a6a66aeae86cae2bcd39e56046557502c059cef6
rianmz-genz <adrianji1945@gmail.com>
2023-08-03 21:15:30 +0700

    create view

--- pages/create.jsx
+++ pages/create.jsx
@@ -1,11 +1,74 @@
+import BreadCumbersCreate from "@/components/createInvitation/BreadCumbersCreate";
+import BrideAndGroomInformation from "@/components/createInvitation/BrideAndGroomInformation";
+import ChooseDesign from "@/components/createInvitation/ChooseDesign";
+import ConfigureDomain from "@/components/createInvitation/ConfigureDomain";
+import LocationInformation from "@/components/createInvitation/LocationInformation";
+import Payment from "@/components/createInvitation/Payment";
 import ProfileCard from "@/components/dashboard/user/profilecard";
-import React from "react";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
 
 export default function Create() {
+  const cucumbersItem = [
+    {
+      label: "Informasi Mempelai",
+      value: "Mempelai",
+    },
+    {
+      label: "Informasi Lokasi",
+      value: "Lokasi",
+    },
+    {
+      label: "Desain Undangan",
+      value: "Desain",
+    },
+    {
+      label: "Atur Domain",
+      value: "Domain",
+    },
+    {
+      label: "Pembayaran",
+      value: "Pembayaran",
+    },
+  ];
+  const [currentMenu, setCurrentMenu] = useState({});
+  useEffect(() => {
+    getCurrentMenu();
+  }, []);
+  const getCurrentMenu = () => {
+    const current = Cookies.get("currentMenu");
+    setCurrentMenu(current ? JSON.parse(current) : cucumbersItem[0]);
+  };
+  const handleClickMenu = (idx) => {
+    const selectedMenu = cucumbersItem.find((item, i) => i == idx);
+    setCurrentMenu(selectedMenu);
+    Cookies.set("currentMenu", JSON.stringify(selectedMenu));
+  };
+  function getComponentView() {
+    switch (currentMenu.value) {
+      case "Mempelai":
+        return <BrideAndGroomInformation />;
+      case "Lokasi":
+        return <LocationInformation />;
+      case "Desain":
+        return <ChooseDesign />;
+      case "Domain":
+        return <ConfigureDomain />;
+      case "Pembayaran":
+        return <Payment />;
+    }
+  }
   return (
     <div className="w-full min-h-screen bg-slate-50 py-12">
+      <ProfileCard />
       <div className="w-11/12 mx-auto">
-        <ProfileCard />
+        <BreadCumbersCreate
+          current={currentMenu}
+          items={cucumbersItem}
+          className={"my-6"}
+          onClick={(idx) => handleClickMenu(idx)}
+        />
+        {getComponentView()}
       </div>
     </div>
   );
