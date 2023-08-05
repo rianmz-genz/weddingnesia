a9f2b109c95ce21187ffe8c67b76c7e5daeed44c
rianmz-genz <adrianji1945@gmail.com>
2023-08-05 21:46:00 +0700

    add padding

--- components/createInvitation/TemplateCreate.js
+++ components/createInvitation/TemplateCreate.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TemplateCreate({ className, children }) {
   return (
-    <div className={`${className} bg-white w-full rounded-md p-4`}>
+    <div className={`${className} bg-white w-full rounded-md p-8`}>
       {children}
     </div>
   );
