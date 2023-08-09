8454ad81fe8177fcf0239bad78ca9a5048f9fb21
rianmz-genz <adrianji1945@gmail.com>
2023-08-09 21:28:53 +0700

    get badge component

---
+++ components/globals/GetBadge.js
@@ -0,0 +1,9 @@
+import Text from "./Text";
+
+export default function GetBadgeInvitation() {
+  return (
+    <div className="px-4 py-2 bg-green-400/20 text-green-400 rounded-md">
+      <Text>Aktif</Text>
+    </div>
+  );
+}
