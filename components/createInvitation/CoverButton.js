c3343d2f077516ab1ab1f61189ff44e8f674b903
rianmz-genz <adrianji1945@gmail.com>
2023-08-17 20:48:31 +0700

    responsif

--- components/createInvitation/CoverButton.js
+++ components/createInvitation/CoverButton.js
@@ -6,19 +6,21 @@ import { buttonStyle, textStyle } from "@/utils/enum";
 
 export default function CoverButton({ title, description, className }) {
   return (
-    <div className={`flex gap-6 w-full ${className}`}>
+    <div
+      className={`flex md:flex-row flex-col gap-2 md:gap-6 w-full ${className}`}
+    >
       <Image
-        className="w-6/12 object-cover rounded-md h-36"
+        className="md:w-6/12 object-cover rounded-md h-36"
         src="/images/wayang.svg"
         width={1080}
         height={1080}
         alt="Wayang"
       />
-      <div className="w-6/12">
+      <div className="md:w-6/12 w-full">
         <Text style={textStyle.description} className={"font-bold"}>
           {title}
         </Text>
-        <Text className={"w-8/12"} style={textStyle.smalldescription}>
+        <Text className={"md:w-8/12"} style={textStyle.smalldescription}>
           {description}
         </Text>
         <Button className={"mt-2"} style={buttonStyle.blackMedium}>
