23cd68e5365ec4a1a7d90baff2e1bff089a17112
rianmz-genz <adrianji1945@gmail.com>
2023-08-20 23:41:28 +0700

    healthy information section

---
+++ components/invitations/blackjavanese/sections/HealthyInformation.js
@@ -0,0 +1,32 @@
+import { Text } from "@/components";
+import { HealthyInformation } from "@/store/demo";
+import { textStyle } from "@/utils/enum";
+import Image from "next/image";
+import React from "react";
+
+export default function HealthyInformationBlackJavanese() {
+  return (
+    <div className=" py-12 w-11/12 mx-auto">
+      <Text style={textStyle.smallTitleAllura} className={"text-center"}>
+        Informasi Protokol Kesehatan
+      </Text>
+      <div className="grid md:grid-cols-3 grid-cols-2 lg:grid-cols-4 gap-1 mt-8 w-full">
+        {HealthyInformation.map(({ src, text, w }, i) => (
+          <div
+            key={i}
+            className="w-full flex flex-col gap-1 justify-start items-center"
+          >
+            <Image
+              src={src}
+              className={w}
+              alt={text}
+              width={1080}
+              height={1080}
+            />
+            <Text className={"text-center"}>{text}</Text>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
