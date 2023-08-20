f0a03f7904356a421cd8ba381a67a744871f6528
rianmz-genz <adrianji1945@gmail.com>
2023-08-20 23:40:29 +0700

    Black Javanese

--- components/invitations/blackjavanese/index.js
+++ components/invitations/blackjavanese/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import HeroBlackJavanese from "./sections/Hero";
+import NavbarBlackJavanese from "./navbar";
+import HealthyInformationBlackJavanese from "./sections/HealthyInformation";
+import BrideBlackJavanese from "./sections/Bride";
+import LocationBlackJavanese from "./sections/Location";
+import GalleryBlackJavanese from "./sections/Gallery";
+import GuestBookBlackJavanese from "./sections/GuestBook";
+import CountdownBlackJavanese from "./sections/Countdown";
 
 export default function BlackJavanese() {
   return (
-    <div>
+    <div className="mb-12">
+      <NavbarBlackJavanese />
       <HeroBlackJavanese />
+      <HealthyInformationBlackJavanese />
+      <CountdownBlackJavanese />
+      <BrideBlackJavanese />
+      <LocationBlackJavanese />
+      <GalleryBlackJavanese />
+      <GuestBookBlackJavanese />
     </div>
   );
 }
