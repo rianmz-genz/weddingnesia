51d1ff1f93af40f4f8939c48361abec2de80b8b4
rianmz-genz <adrianji1945@gmail.com>
2023-08-20 23:42:00 +0700

    demo invitation page

--- pages/demo/invitation.jsx
+++ pages/demo/invitation.jsx
@@ -1,11 +1,11 @@
 import { initialValue } from "@/store";
+import DemoInvitationData from "@/store/demo";
 import { useRouter } from "next/router";
 import React from "react";
 export default function DemoInvitation() {
   const router = useRouter();
-  const { demoInvitations } = initialValue;
   const { sl } = router.query;
-  const currentInvitation = demoInvitations.find((item) => item.slug == sl);
+  const currentInvitation = DemoInvitationData.find((item) => item.slug == sl);
   if (!currentInvitation) {
     // Jika currentInvitation adalah undefined, kembalikan halaman 404.
     return <NotFoundMessage />; // Ganti ini dengan komponen 404 yang sesuai
