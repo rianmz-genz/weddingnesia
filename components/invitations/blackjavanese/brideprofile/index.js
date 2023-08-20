1ad6a2aa76b9296a2ddafc1966c5e361d7510f3a
rianmz-genz <adrianji1945@gmail.com>
2023-08-20 23:40:42 +0700

    bride profile component

---
+++ components/invitations/blackjavanese/brideprofile/index.js
@@ -0,0 +1,30 @@
+import { Text } from "@/components";
+import { textStyle } from "@/utils/enum";
+import Image from "next/image";
+import React from "react";
+
+export default function BrideProfile({
+  src,
+  nickName,
+  fullName,
+  father,
+  mother,
+}) {
+  return (
+    <div className="md:w-6/12 w-full flex flex-col items-center">
+      <Image
+        src={src}
+        width={1080}
+        height={1080}
+        alt={`Foto ${nickName}`}
+        className="rounded-full w-64 h-64 object-cover mb-3 ring-yellow-600 ring-2"
+      />
+      <Text style={textStyle.smallDescriptionAllura}>-{nickName}-</Text>
+      <Text style={textStyle.descriptionAllura}>{fullName}</Text>
+      <Text className={"mt-1"}>Putra dari pasangan</Text>
+      <Text>
+        Bapak {father} & Ibu {mother}
+      </Text>
+    </div>
+  );
+}
