bb6766fa91bdf1f2a51b345bca7ffe6444a846f1
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-08-23 19:09:50 +0700

    next auth config

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import InstagramProvider from "next-auth/providers/instagram";
+
+export const authOptions = {
+  pages: {
+    signIn: '/auth/signin',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    }),
+
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      console.log("user");
+      console.log(user);
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
