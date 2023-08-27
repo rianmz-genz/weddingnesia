ae366da362d7de6a9b1d5e46ce0c471b4866e9a4
rianmz-genz <adrianji1945@gmail.com>
2023-08-27 11:03:44 +0700

    home page

--- components/home/Price/index.js
+++ components/home/Price/index.js
@@ -13,14 +13,14 @@ export default function PriceSection() {
   return (
     <section
       id="price"
-      className="bg-slate-50 sm:max-w-lg py-28 lg:max-w-7xl px-4"
+      className="bg-slate-50 sm:max-w-lg lg:max-w-7xl mx-auto py-16 lg:py-32 px-7"
     >
       <SectionParagraph
-        className={"w-8/12 text-center mx-auto"}
+        className={"lg:w-8/12 text-center mx-auto"}
         title={pricing.title}
         description={pricing.description}
       />
-      <ul className="grid grid-cols-5 mt-12 gap-3">
+      <ul className="grid grid-cols-1 lg:grid-cols-5 mt-12 gap-3 place-items-center">
         {benefits.map(({ data }, i) => (
           <PriceCard
             key={i}
@@ -28,6 +28,7 @@ export default function PriceSection() {
             benefits={data.benefits}
             name={data.name}
             price={data.price}
+            slug={data.slug}
           />
         ))}
       </ul>
@@ -37,8 +38,15 @@ export default function PriceSection() {
 
 const PriceCard = ({ src, name, price, slug, benefits }) => {
   const thePrice = price.toLocaleString("id-ID").split(".");
+
   return (
-    <li className="bg-white shadow rounded-md px-4 py-4 w-full text-center">
+    <li
+      className={`${
+        slug == "eksklusif"
+          ? "bg-black text-white shadow-2xl shadow-black/70 hover:shadow-xl transition-all duration-500"
+          : "shadow bg-white z-30"
+      }  rounded-xl px-4 py-8 w-full text-center h-fit`}
+    >
       <Image
         src={src}
         alt={name}
@@ -46,7 +54,7 @@ const PriceCard = ({ src, name, price, slug, benefits }) => {
         height={1080}
         className="w-16 mx-auto"
       />
-      <Text style={textStyle.smalltitle} className={"font-semibold mt-5 mb-2"}>
+      <Text style={textStyle.smalltitle} className={`font-semibold mt-5 mb-2 `}>
         {name}
       </Text>
       <div>
@@ -68,15 +76,23 @@ const PriceCard = ({ src, name, price, slug, benefits }) => {
           </span>
         )}
       </div>
-      <ul className="w-full flex flex-col gap-3 mb-8 mt-6">
+      <ul className={`"w-full flex flex-col gap-3 mb-8 mt-6`}>
         {benefits.map((item, i) => (
           <li className="text-black/80 items-center  flex gap-2" key={i}>
             <AiOutlineCheckCircle className="text-green-500" />
-            <p className="text-sm">{item}</p>
+            <p className={`text-sm ${slug == "eksklusif" ? "text-white" : ""}`}>
+              {item}
+            </p>
           </li>
         ))}
       </ul>
-      <Button style={buttonStyle.blackLarge}>Buat Undangan</Button>
+      <Button
+        style={
+          slug == "eksklusif" ? buttonStyle.whitelarge : buttonStyle.blackLarge
+        }
+      >
+        Buat Undangan
+      </Button>
     </li>
   );
 };
