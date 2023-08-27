b58080e3f4db4ac6d2deb7ea301531c6ba5c189a
rianmz-genz <adrianji1945@gmail.com>
2023-08-27 11:03:53 +0700

    home page

--- components/home/Tutorial/index.js
+++ components/home/Tutorial/index.js
@@ -9,15 +9,15 @@ const TutrialSection = () => {
   const { tutorial } = initialValue.home;
   return (
     <section className="w-full flex items-center justify-start h-fit py-28">
-      <div className="md:w-8/12 w-11/12">
+      <div className="lg:w-8/12 w-11/12">
         <SectionParagraph
           title={tutorial.title}
           description={tutorial.description}
         />
-        <ol className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+        <ol className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
           {tutorial.items.map((item, idx) => (
             <li key={idx} className="relative">
-              <div className="px-6 py-8 rounded-md h-full ring-yellow-600 before:-z-10 before:absolute div-to-match-height before:bg-yellow-600 before:-bottom-1 before:left-1 before:w-full before:h-full before:rounded-md ring-1 bg-white">
+              <div className="px-6 py-8 rounded-lg h-full ring-yellow-600 before:-z-10 before:absolute div-to-match-height before:bg-yellow-600 before:-bottom-1 before:left-1 before:w-full before:h-full before:rounded-lg ring-1 bg-white">
                 <div className="flex items-center space-x-2 mb-2 ">
                   <Text style={textStyle.title} className={"text-yellow-600"}>
                     {item.icon}
@@ -37,7 +37,7 @@ const TutrialSection = () => {
           ))}
         </ol>
       </div>
-      <div className="md:w-4/12 hidden md:block relative">
+      <div className="lg:w-4/12 hidden lg:block relative">
         <Image
           src={initialValue.home.tutorial.img}
           className="w-full scale-[200%] top-0 left-1/2 absolute"
