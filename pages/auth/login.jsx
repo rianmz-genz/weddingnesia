70c1f1e47eaa11b10b53bf85113b1ac75a0c1510
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-08-30 00:09:15 +0700

    login form

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -9,6 +9,8 @@ import { FiKey } from "react-icons/fi";
 import GoogleIcon from "@/public/images/google.png";
 import FacebookIcon from "@/public/images/Facebook.png";
 import InstagramIcon from "@/public/images/instagram.png";
+import LoginApi from "@/api/auth/LoginApi";
+import Cookies from "js-cookie";
 
 function ProviderButton(provider) {
   let icon;
@@ -38,9 +40,22 @@ function ProviderButton(provider) {
 
 const LoginView = () => {
   const router = useRouter();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorEmail, setErrorEmail] = useState("");
+
   const handleLogin = (e) => {
     e.preventDefault();
-    router.push("/dashboard");
+    LoginApi({email, password}).then((res) => {
+      if (res.errors != null) {
+        console.log('error');
+        setErrorEmail(res.errors.email)
+      } else {
+        Cookies.set("token", res.access_token, { expires: 2 });
+      }
+    })
+
+    //router.push("/dashboard");
   };
   const [loginUrl, setLoginUrl] = useState(null);
 
@@ -102,14 +117,18 @@ const LoginView = () => {
           autoFocus={true}
           placeholder="Email"
           icon={<BiUser className="text-black mr-2" />}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <Text className={"mb-1 mt-3"} style={textStyle.titleQuestion}>
+        {errorEmail ? <div className="text-sm text-red-500">{errorEmail}</div> : ''}
+        <Text className={"mb-1 mt-3"}
+              style={textStyle.titleQuestion}>
           Password
         </Text>
         <InputIcon
           type="password"
           placeholder="Password"
           icon={<FiKey className="text-black mr-2" />}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <Button className={"mt-6 w-full"}>Masuk</Button>
         <Text className={"text-center mt-3"}>
