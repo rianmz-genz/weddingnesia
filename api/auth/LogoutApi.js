fc7e9ad9eda564dd7ee1439ddf9feb64a7833ea6
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-08-31 00:11:34 +0700

    logout api

---
+++ api/auth/LogoutApi.js
@@ -0,0 +1,17 @@
+import { urlLogout } from "@ApiRoutes/auth";
+import Cookies from "js-cookie";
+
+const LogoutApi = async () => {
+  const token = "Bearer " + Cookies.get("token");
+  return await fetch(urlLogout, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    },
+  }).then((response) => {
+    return response.json()
+  })
+};
+
+export default LogoutApi;
