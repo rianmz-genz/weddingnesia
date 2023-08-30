69f713e5e9aeb47bd35506eb1423f2752f6379e2
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-08-30 20:41:25 +0700

    api routes

---
+++ api/routes/auth/index.js
@@ -0,0 +1,4 @@
+import GetDomain from "@/api/utils/GetDomain";
+export const urlLogin = `${GetDomain()}/login`;
+export const urlRegister = `${GetDomain()}/register`;
+export const urlVerify = `${GetDomain()}/verify-email`;
