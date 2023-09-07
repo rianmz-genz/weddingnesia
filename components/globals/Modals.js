0afe0d7062afb58430b2b9f0b928382a72006d14
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-09-07 22:24:48 +0700

    add trigger

--- components/globals/Modals.js
+++ components/globals/Modals.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export default function Modals({ children, onSubmit }) {
+export default function Modals({ children, trigger, onClose }) {
+  const divRef = useRef(null);
+  const [isOpened, setIsOpened] = useState(false);
+  useEffect(() => {
+    setIsOpened(trigger);
+  }, [trigger]);
   return (
-    <div className="w-full h-screen bg-black/40 z-50 backdrop-blur-sm fixed flex justify-center items-center">
-      <form
-        onSubmit={onSubmit}
+    <div
+      onClick={onClose}
+      className={`${
+        isOpened ? "visible opacity-100" : "invisible opacity-0"
+      } transition-all duration-500 w-full h-screen bg-black/40 z-50 backdrop-blur-sm fixed flex justify-center items-center`}
+    >
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        ref={divRef}
         className="max-w-lg w-full px-8 py-4 bg-white rounded-lg"
       >
         {children}
-      </form>
+      </div>
     </div>
   );
 }
