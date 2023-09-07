3ba4c7d363c48b07c753497002a768701de8fb11
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-09-07 19:34:01 +0700

    api get detial invitation

--- components/globals/DetailInvitationAction.js
+++ components/globals/DetailInvitationAction.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BiRefresh } from "react-icons/bi";
 import { FiCheckCircle, FiEdit, FiTrash } from "react-icons/fi";
 import { RiShareCircleLine } from "react-icons/ri";
-export default function DetailInvitationAction() {
+
+export default function DetailInvitationAction({ guestId, openDelete }) {
   return (
     <ul className="flex w-full my-2 flex-wrap gap-2 transition-all duration-300">
       <IconButton className={"hover:bg-green-500/10 hover:text-green-500"}>
@@ -11,22 +12,24 @@ export default function DetailInvitationAction() {
       <IconButton className={"hover:bg-orange-500/10 hover:text-orange-500"}>
         <BiRefresh />
       </IconButton>
+      
       <IconButton className={"hover:bg-indigo-500/10 hover:text-indigo-500"}>
         <RiShareCircleLine />
       </IconButton>
       <IconButton className={"hover:bg-blue-500/10 hover:text-blue-500"}>
         <FiEdit />
       </IconButton>
-      <IconButton className={"hover:bg-red-500/10 hover:text-red-500"}>
+      <IconButton onClick={openDelete} className={"hover:bg-red-500/10 hover:text-red-500"}>
         <FiTrash />
       </IconButton>
     </ul>
   );
 }
 
-const IconButton = ({ className, children }) => {
+const IconButton = ({ className, children, onClick }) => {
   return (
     <li
+      onClick={onClick}
       className={`transition-all duration-300 cursor-pointer rounded-md text-lg p-1 ${className}`}
     >
       {children}
