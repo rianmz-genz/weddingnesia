a92179ba4692f2ee4edab8859d3bd815a316cf1f
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-09-07 19:33:54 +0700

    api get guests by invitation id

---
+++ api/integrations/invitation/guest/GetGuestsByInvitationId.js
@@ -0,0 +1,17 @@
+import GetDomain from "@/api/utils/GetDomain";
+import GetToken from "@/api/utils/GetToken";
+
+const GetGuestsByInvitationId = async (invitationId) => {
+    const url = `${GetDomain()}/invitations/${invitationId}/guests`
+    return await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": GetToken()
+        }
+    }).then((response) => {
+        return response.json()
+    }).catch((error) => console.log("on error" + error))
+}
+
+export default GetGuestsByInvitationId
