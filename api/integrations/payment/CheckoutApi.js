561c9e136607a6977fd08cd6a9c58d871a7bd7fa
rianmz-genz <adrianji1945@gmail.com>
2023-09-08 08:35:43 +0700

    checkout

--- api/integrations/payment/CheckoutApi.js
+++ api/integrations/payment/CheckoutApi.js
@@ -1,20 +1,19 @@
 import { urlCheckout } from "@/api/routes/payment";
 
 const CheckoutApi = async ({ orderId }) => {
-  const data = {"order_id": orderId}
-    
+  const data = { order_id: orderId };
+
   const response = await fetch(urlCheckout, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-    
   }).then((response) => {
-    return response.json()
-  })
-  
-  return response
+    return response.json();
+  });
+
+  return response;
 };
 
 export default CheckoutApi;
