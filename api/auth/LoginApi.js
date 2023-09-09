096c7361f809f5380612b00276a407e44d0b7884
rianmz-genz <adrianji1945@gmail.com>
2023-09-09 14:19:06 +0700

    login

--- api/auth/LoginApi.js
+++ api/auth/LoginApi.js
@@ -2,19 +2,19 @@ import { urlLogin } from "@ApiRoutes/auth";
 
 const LoginApi = async ({ email, password }) => {
   const data = { email, password };
-    
+
   const response = await fetch(urlLogin, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Accept: "application/json",
     },
     body: JSON.stringify(data),
-    
   }).then((response) => {
-    return response.json()
-  })
-  
-  return response
+    return response.json();
+  });
+
+  return response;
 };
 
 export default LoginApi;
