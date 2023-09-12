07dea0c7ee2446c10bf4755ab60c7e5cf5ca5fab
rianmz-genz <adrianji1945@gmail.com>
2023-09-12 11:03:53 +0700

    countdown

--- components/invitations/blackjavanese/sections/Countdown.js
+++ components/invitations/blackjavanese/sections/Countdown.js
@@ -1,24 +1,62 @@
 import { Text } from "@/components";
+import { InvitationContext } from "@/context/invitation";
+import { dateNow } from "@/utils";
 import { textStyle } from "@/utils/enum";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 export default function CountdownBlackJavanese() {
+  const { wedding_date } = useContext(InvitationContext);
+  const now = wedding_date ?? new Date();
+  const targetDate = new Date(now).getTime();
+  const [countdown, setCountdown] = useState({
+    days: "",
+    hours: "",
+    minutes: "",
+    seconds: "",
+  });
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    const timer = setInterval(() => {
+      setCountdown(calculateCountdown());
+    }, 1000);
+
+    return () => {
+      setIsMounted(false);
+      clearInterval(timer);
+    };
+  }, [wedding_date]);
+
+  function calculateCountdown() {
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  }
   const countdownData = [
     {
       label: "Hari",
-      value: "00",
+      value: countdown.days,
     },
     {
       label: "Jam",
-      value: "00",
+      value: countdown.hours,
     },
     {
       label: "Menit",
-      value: "00",
+      value: countdown.minutes,
     },
     {
       label: "Detik",
-      value: "00",
+      value: countdown.seconds,
     },
   ];
   return (
@@ -27,9 +65,10 @@ export default function CountdownBlackJavanese() {
         Menghitung Hari
       </Text>
       <ul className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 w-11/12 mx-auto gap-3 mt-6">
-        {countdownData.map(({ label, value }, i) => (
-          <CountdownCard key={i} label={label} value={value} />
-        ))}
+        {isMounted &&
+          countdownData.map(({ label, value }, i) => (
+            <CountdownCard key={i} label={label} value={value} />
+          ))}
       </ul>
     </div>
   );
