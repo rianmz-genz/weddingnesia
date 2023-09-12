15a28a34b8f61f7fe19dd14e9683acd43cd3450b
rianmz-genz <adrianji1945@gmail.com>
2023-09-12 11:04:13 +0700

    [name]

--- pages/dashboard/invitations/[name].jsx
+++ pages/dashboard/invitations/[name].jsx
@@ -59,10 +59,11 @@ export default function InvitationsDetail() {
       top: "Link",
       bottom: (
         <Link
-          href={`/invitation?sl=${router.query.name}`}
+          href={`/theme?sl=blackjavanese&i=${router.query.name}`}
           className="italic underline"
         >
-          https://app.weddingnesia.id/invitation?sl={router.query.name}
+          https://app.weddingnesia.id/theme?sl=blackjavanese&i=
+          {router.query.name}
         </Link>
       ),
     },
