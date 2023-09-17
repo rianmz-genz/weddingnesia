060496f044f54b9a2357e49dbb9cea63f714ee1a
rianmz-genz <adrianji1945@gmail.com>
2023-09-17 22:42:54 +0700

    tiga

--- components/invitations/blackjavanese/sections/Bride.js
+++ components/invitations/blackjavanese/sections/Bride.js
@@ -14,29 +14,31 @@ export default function BrideBlackJavanese() {
     groom_name,
     bride_name,
     groom_fullname,
+    groom_info,
     bride_fullname,
+    bride_info,
   } = useContext(InvitationContext);
 
   return (
-    <div id="bride" className="w-11/12 mx-auto py-12 text-center">
-      <Text style={textStyle.smallTitleAllura}>Kami Yang Berbahagia</Text>
-      <Text className={"my-3 font-bold"}>{opening_remarks}</Text>
-      <Text>{greeting}</Text>
-      <div className="flex justify-center md:flex-row flex-col gap-6 w-full mt-12">
-        <BrideProfile
-          src={groom_avatar}
-          nickName={groom_name}
-          fullName={groom_fullname}
-          father={"Sudjaman"}
-          mother={"Sri Mulyani"}
-        />
-        <BrideProfile
-          src={bride_avatar}
-          nickName={bride_name}
-          fullName={bride_fullname}
-          father={"Sutiman"}
-          mother={"Sudjanti"}
-        />
+    <div id="bride" className=" py-12 w-full bg-slate-50">
+      <div className="w-11/12 text-center mx-auto">
+        <Text style={textStyle.smallTitleAllura}>Kami Yang Berbahagia</Text>
+        <Text className={"my-3 font-bold"}>{opening_remarks}</Text>
+        <Text>{greeting}</Text>
+        <div className="flex justify-center md:flex-row flex-col gap-6 w-full mt-12">
+          <BrideProfile
+            src={groom_avatar}
+            nickName={groom_name}
+            fullName={groom_fullname}
+            info={groom_info}
+          />
+          <BrideProfile
+            src={bride_avatar}
+            nickName={bride_name}
+            fullName={bride_fullname}
+            info={bride_info}
+          />
+        </div>
       </div>
     </div>
   );
