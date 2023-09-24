07eb7451d25a002162f2f9bf98d7f54c21c90f00
rianmz-genz <adrianji1945@gmail.com>
2023-09-24 22:16:46 +0700

    api

--- api/integrations/invitation/guest/GetGuestsByInvitationId.js
+++ api/integrations/invitation/guest/GetGuestsByInvitationId.js
@@ -2,16 +2,18 @@ import GetDomain from "@/api/utils/GetDomain";
 import GetToken from "@/api/utils/GetToken";
 
 const GetGuestsByInvitationId = async (invitationId) => {
-    const url = `${GetDomain()}/invitations/${invitationId}/guests`
-    return await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": GetToken()
-        }
-    }).then((response) => {
-        return response.json()
-    }).catch((error) => console.log("on error" + error))
-}
+  const url = `${GetDomain()}/invitations/${invitationId}/guests`;
+  return await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: GetToken(),
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log("on error" + error));
+};
 
-export default GetGuestsByInvitationId
+export default GetGuestsByInvitationId;
--- api/package/GetPackages.js
+++ api/package/GetPackages.js
@@ -4,16 +4,16 @@ const GetPackagesApi = async () => {
   try {
     const res = await axios({
       method: "GET",
-      baseURL: 'http://127.0.0.1:8000/api/package',
+      baseURL: "http://127.0.0.1:8000/api/package",
       timeout: 5000,
       timeoutErrorMessage: "Request time out, coba lagi",
     });
     return res.data;
   } catch (error) {
     if (error.code === "ECONNABORTED") {
-      console.log(error.message);
+      //console.log(error.message);
     } else {
-      console.log(error);
+      //console.log(error);
       return error.response.data;
     }
   }
