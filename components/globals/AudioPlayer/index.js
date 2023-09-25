9790be0f45d94541ad6c58c1e83225f643c0ba70
rianmz-genz <adrianji1945@gmail.com>
2023-09-25 23:34:00 +0700

    audio player

--- components/globals/AudioPlayer/index.js
+++ components/globals/AudioPlayer/index.js
@@ -3,7 +3,7 @@ import { FaPlay, FaPause } from "react-icons/fa";
 
 const AudioPlayer = ({ audioLink }) => {
   return (
-    <div className="fixed bottom-12 lg:bottom-20 left-3 z-50">
+    <div className="fixed bottom-6 lg:bottom-3 left-3 z-50">
       <iframe width="150" height="75" allow="autoplay" src={audioLink}></iframe>
     </div>
   );
