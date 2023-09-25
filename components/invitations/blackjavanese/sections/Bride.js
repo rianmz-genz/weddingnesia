361699f1263436bc2a4429e0a3e7e8efca6f9d26
rianmz-genz <adrianji1945@gmail.com>
2023-09-25 23:34:23 +0700

    component

--- components/invitations/blackjavanese/sections/Bride.js
+++ components/invitations/blackjavanese/sections/Bride.js
@@ -1,10 +1,11 @@
 import { Text } from "@/components";
 import { textStyle } from "@/utils/enum";
 import Image from "next/image";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import BrideProfile from "../brideprofile";
 import { InvitationContext } from "@/context/invitation";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function BrideBlackJavanese() {
   const {
     opening_remarks,
@@ -20,13 +21,38 @@ export default function BrideBlackJavanese() {
     bride_instagram,
     groom_instagram,
   } = useContext(InvitationContext);
-
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div id="bride" className=" py-12 w-full bg-slate-50">
+    <div id="bride" className=" py-16 w-full bg-gray-100">
       <div className="w-11/12 text-center mx-auto">
-        <Text style={textStyle.smallTitleAllura}>Kami Yang Berbahagia</Text>
-        <Text className={"my-3 font-bold"}>{greeting}</Text>
-        <Text className={"w-6/12 mx-auto"}>{opening_remarks}</Text>
+        <Image
+          data-aos-duration="1000"
+          data-aos="fade-up"
+          src={"/images/flower-2.png"}
+          alt="BUnga"
+          width={1080}
+          height={1080}
+          className="w-64 mb-3 mx-auto"
+        />
+        <Text
+          data-aos-duration="1000"
+          data-aos="fade-up"
+          style={textStyle.smallTitleAllura}
+        >
+          Kami Yang Berbahagia
+        </Text>
+        <Text data-aos-duration="1000" data-aos="fade-up" className={"my-3"}>
+          {greeting}
+        </Text>
+        <Text
+          data-aos-duration="1000"
+          data-aos="fade-up"
+          className={"w-6/12 mx-auto"}
+        >
+          {opening_remarks}
+        </Text>
         <div className="flex justify-center md:flex-row flex-col gap-12 w-full mt-12">
           <BrideProfile
             src={groom_avatar}
