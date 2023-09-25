235e30ec9c3ce98eb4b4aa4c106376ee8f291f2d
rianmz-genz <adrianji1945@gmail.com>
2023-09-25 23:34:47 +0700

    component

--- components/invitations/blackjavanese/sections/Quotes.js
+++ components/invitations/blackjavanese/sections/Quotes.js
@@ -2,17 +2,26 @@ import Text from "@/components/globals/Text";
 import { InvitationContext } from "@/context/invitation";
 import { textStyle } from "@/utils/enum";
 import React, { useContext } from "react";
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function QuotesBlackJavanese() {
   const { secondary_cover, quotes, source_quotes } =
     useContext(InvitationContext);
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div
       className={`w-full h-fit bg-cover bg-center mb-20 flex justify-center items-center`}
       style={{ backgroundImage: `url(${secondary_cover})` }}
     >
       <div className="w-full min-h-48 py-12 bg-black/40  text-white">
-        <div className="mx-auto w-6/12">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          className="mx-auto w-6/12 flex flex-col justify-center items-center"
+        >
           <Text style={textStyle.title}>{quotes}</Text>
           <Text style={textStyle.descriptionAllura}>-{source_quotes}-</Text>
         </div>
