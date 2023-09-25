3632dcfb05110daa069dc8601c43a08d8422761d
rianmz-genz <adrianji1945@gmail.com>
2023-09-25 23:40:46 +0700

    fix

--- components/globals/AudioPlayer/index.js
+++ components/globals/AudioPlayer/index.js
@@ -3,8 +3,14 @@ import { FaPlay, FaPause } from "react-icons/fa";
 
 const AudioPlayer = ({ audioLink }) => {
   return (
-    <div className="fixed bottom-6 lg:bottom-3 left-3 z-50">
-      <iframe width="150" height="75" allow="autoplay" src={audioLink}></iframe>
+    <div className="fixed bottom-4 lg:bottom-3 left-3 z-40">
+      <iframe
+        width="150"
+        height="75"
+        className="lg:w-[150px] w-[100px] lg:h-[75px] h-[60px]"
+        allow="autoplay"
+        src={audioLink}
+      ></iframe>
     </div>
   );
 };
