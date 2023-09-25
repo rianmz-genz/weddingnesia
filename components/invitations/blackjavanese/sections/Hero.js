c01b2043dd8c15f093b06ac71316253120adfa09
rianmz-genz <adrianji1945@gmail.com>
2023-09-25 23:34:32 +0700

    component

--- components/invitations/blackjavanese/sections/Hero.js
+++ components/invitations/blackjavanese/sections/Hero.js
@@ -3,6 +3,7 @@ import Text from "@/components/globals/Text";
 import { InvitationContext } from "@/context/invitation";
 import { formatDate } from "@/utils";
 import { textStyle } from "@/utils/enum";
+import Image from "next/image";
 import React, { useContext } from "react";
 
 export default function HeroBlackJavanese() {
@@ -10,7 +11,7 @@ export default function HeroBlackJavanese() {
     useContext(InvitationContext);
   return (
     <div
-      className={`w-full min-h-screen bg-cover bg-center mb-12`}
+      className={`w-full min-h-screen bg-cover bg-center`}
       style={{ backgroundImage: `url(${primary_cover})` }}
     >
       <AudioPlayer
@@ -19,7 +20,14 @@ export default function HeroBlackJavanese() {
         }
       />
       <div className="w-full min-h-screen bg-black/60 flex justify-center items-center">
-        <div className="flex flex-col items-center gap-2 text-white w-11/12">
+        <div className="flex flex-col items-center gap-1 text-white w-11/12">
+          <Image
+            src={"/images/flower.png"}
+            alt="Kembang"
+            width={1080}
+            height={1080}
+            className="w-24 mb-3"
+          />
           <Text style={textStyle.description}>Welcome to Our Wedding</Text>
           <Text style={textStyle.heroTitleAllura}>
             {groom_name} & {bride_name}
