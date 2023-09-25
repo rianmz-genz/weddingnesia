cff404a0bfe53e827804ca960ac4790a7a15bd93
rianmz-genz <adrianji1945@gmail.com>
2023-09-25 11:58:37 +0700

    doc

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,23 +3,6 @@ import Script from "next/script";
 import { useEffect } from "react";
 
 export default function Document() {
-  useEffect(() => {
-    const handleContextMenu = (e) => {
-      // prevent the right-click menu from appearing
-      //console.log(e);
-      e.preventDefault();
-    };
-
-    // attach the event listener to
-    // the document object
-    document.addEventListener("contextmenu", handleContextMenu);
-
-    // clean up the event listener when
-    // the component unmounts
-    return () => {
-      document.removeEventListener("contextmenu", handleContextMenu);
-    };
-  }, []);
   return (
     <Html lang="en">
       <Head>
