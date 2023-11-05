a3ba4132c5684775eeef2888e1f105f210cb3e63
rianmz-genz <adrianji1945@gmail.com>
2023-11-05 23:51:07 +0700

    inv item

--- components/dashboard/user/invitation/InvitationItem.js
+++ components/dashboard/user/invitation/InvitationItem.js
@@ -1,29 +1,26 @@
-const {
-  default: GetBadgeInvitation,
-} = require("@/components/globals/GetBadge");
-const { default: Text } = require("@/components/globals/Text");
-const { textStyle } = require("@/utils/enum");
-const { default: Link } = require("next/link");
-const { FiEye, FiEdit } = require("react-icons/fi");
-
-export default function InvitationItem({
-  groom_name,
-  bride_name,
-  slug,
-  status,
-}) {
+import GetBadgeInvitation from "@/components/globals/GetBadge";
+import Text from "@/components/globals/Text";
+import { textStyle } from "@/utils/enum";
+import Cookies from "js-cookie";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FiEdit, FiEye } from "react-icons/fi";
+export default function InvitationItem({ slug, status, id }) {
+  const router = useRouter();
+  const onClickEdit = () => {
+    Cookies.set("tempId", id);
+    router.push("/create");
+  };
   return (
     <li className="w-full bg-white shadow-sm px-4 py-2 rounded-md flex justify-between items-center">
-      <Text style={textStyle.description}>
-        {groom_name} dan {bride_name}
-      </Text>
+      <Text style={textStyle.description}>{slug}</Text>
       <div className="flex items-center justify-center space-x-4 text-xl">
         <Link href={`/dashboard/invitations/${slug}`}>
           <FiEye />
         </Link>
-        <Link href={"#"}>
+        <button onClick={onClickEdit}>
           <FiEdit />
-        </Link>
+        </button>
         <GetBadgeInvitation status={status} />
       </div>
     </li>
